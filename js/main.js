766cd16de08a5cb09a8b0f5ef6c915ec60787a90
tatiuska <tfweska@gmail.com>
2024-03-05 21:25:42 -0300

    criação e customização do carrossel com depoimentos dos clientes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-// Função para filtrar os projetos de acordo com a categoria
+$(document).ready(function(){
 
-// init Isotope
-let $grid = $('.grid').isotope({
-    // options
-});
+    // Função para filtrar os projetos de acordo com a categoria
+    // init Isotope
+    let $grid = $('.grid').isotope({
+        // options
+    });
 
-// filter items on button click
-$('.button-group').on('click', 'button', function() {
-    let filterValue = $(this).attr('data-filter');
-    $grid.isotope({filter : filterValue});
-});
+    // filter items on button click
+    $('.button-group').on('click', 'button', function() {
+        let filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter : filterValue});
+    });
 
-// Magnific Popup code
-$('.test-popup-link').magnificPopup({
-    type: 'image',
-    gallery:{enabled:true}
+    // Magnific Popup code
+    $('.test-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+    });
+
+    // Owl carousel initialization
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            544: {
+                items: 3
+            }
+        }
+    });
 });
