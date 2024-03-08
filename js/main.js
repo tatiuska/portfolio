d4df772978d71e00d4986ec3421530a69e43922f
tatiuska <tfweska@gmail.com>
2024-03-08 16:13:26 -0300

    criação função navbarFixed e classe global .navbar-fixed, para que o menu de navegação fique fixo ao navegar para baixo no site.

--- js/main.js
+++ js/main.js
@@ -32,4 +32,23 @@ $(document).ready(function(){
             }
         }
     });
+
+    // sticky navigation menu
+    let nav_offset_top = $('.header-area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header-area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header-area .main-menu').addClass('navbar-fixed');
+                } else {
+                    $('.header-area .main-menu').removeClass('navbar-fixed');
+                }
+            });
+        };
+    };
+
+    navbarFixed();
+    
 });
